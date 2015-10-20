dd6ac1b38302eb7490e03e82b850d46279886dec
hide_m <hide_m@ym.tose.co.jp>
2015-10-20 14:37:40 +0900

    P233:7.1.3.3　要素の移動あるいは複製.

--- CalendarEvent.cs
+++ CalendarEvent.cs
@@ -6,19 +6,10 @@ using System.Threading.Tasks;
 
 namespace ch07_01
 {
-	class CalendarEvent : IComparable<CalendarEvent>
+	class CalendarEvent
 	{
 		public string Title { get; set; }
 		public DateTimeOffset StartTime { get; set; }
 		public TimeSpan Duration { get; set; }
-
-		public int CompareTo(CalendarEvent other)
-		{
-			if (other == null)
-			{
-				return 1;
-			}
-			return StartTime.CompareTo(other.StartTime);
-		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -10,69 +10,7 @@ namespace ch07_01
 	{
 		static void Main(string[] args)
 		{
-			CalendarEvent[] events = SeparateEvents();
-
-
-			LoopFind(events);
-			FindAll(events);
-			SortEvents(events);
-
-			int[] numbers = { 4, 1, 2, 5, 3 };
-			Array.Sort(numbers);
-			Array.Sort(events);
-
-			Console.ReadKey();
-		}
-
-		static void LoopFind(CalendarEvent[] events)
-		{
-			DateTime dateOfInterest = new DateTime(2009, 7, 12);
-			foreach (CalendarEvent item in events)
-			{
-				if (item.StartTime.Date == dateOfInterest)
-				{
-					Console.WriteLine(item.Title + ": " + item.StartTime);
-				}
-			}
-			Console.WriteLine();
-		}
-
-		static void FindAll(CalendarEvent[] events)
-		{
-			DateTime dateOfInterest = new DateTime(2009, 7, 12);
-			CalendarEvent[] itemsOnDateOfInterest =
-				Array.FindAll(events, e => e.StartTime.Date == dateOfInterest);
-			foreach (CalendarEvent item in itemsOnDateOfInterest)
-			{
-				Console.WriteLine(item.Title + "：" + item.StartTime);
-			}
-			Console.WriteLine();
-		}
-
-		static void SortEvents(CalendarEvent[] events)
-		{
-			Array.Sort(events,
-				(event1, event2) => event1.StartTime.CompareTo(event2.StartTime));
-			PrintEvents(events);
-			Console.WriteLine();
-
-			Array.Sort(events,
-				(event1, event2) => event1.Duration.CompareTo(event2.Duration));
-			PrintEvents(events);
-			Console.WriteLine();
-		}
-
-		static void PrintEvents(CalendarEvent[] events)
-		{
-			foreach (CalendarEvent item in events)
-			{
-				Console.WriteLine(item.Title + ": " + item.StartTime);
-			}
-		}
-
-		private static CalendarEvent[] SeparateEvents()
-		{
-			CalendarEvent[] events =
+			CalendarEvent[] events1 =
 			{
 				new CalendarEvent
 				{
@@ -86,6 +24,9 @@ namespace ch07_01
 					StartTime = new DateTimeOffset (2009, 7, 11, 19, 30, 00, TimeSpan.Zero),
 					Duration = TimeSpan.FromHours(6.5)
 				},
+			};
+			CalendarEvent[] events2 =
+			{
 				new CalendarEvent
 				{
 					Title = "F1 ドイツグランプリ",
@@ -105,7 +46,45 @@ namespace ch07_01
 					Duration = TimeSpan.FromHours(5)
 				}
 			};
-			return events;
+
+			CalendarEvent[] allEvents = CombineEvents(events1, events2);
+			PrintEvents(allEvents);
+			Console.WriteLine();
+			PrintEvents(RemoveFirstEvent(allEvents));
+			Console.ReadKey();
 		}
+
+		static void PrintEvents(CalendarEvent[] events)
+		{
+			foreach (CalendarEvent item in events)
+			{
+				Console.WriteLine(item.Title + ": " + item.StartTime);
+			}
+		}
+
+		static CalendarEvent[] CombineEvents(CalendarEvent[] event1,
+											 CalendarEvent[] event2)
+		{
+			CalendarEvent[] combinedEvents =
+				new CalendarEvent[event1.Length + event2.Length];
+			event1.CopyTo(combinedEvents, 0);
+			event2.CopyTo(combinedEvents, event1.Length);
+
+			return combinedEvents;
+		}
+
+		static CalendarEvent[] RemoveFirstEvent(CalendarEvent[] events)
+		{
+			CalendarEvent[] croppedEvents = new CalendarEvent[events.Length - 1];
+			Array.Copy(
+				events,
+				1,
+				croppedEvents,
+				0,
+				events.Length - 1
+				);
+			return croppedEvents;
+		}
+
 	}
 }
